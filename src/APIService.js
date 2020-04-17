6de3227386ec90c5066d68e98737f48b719054a7
NassrEML <nassr906@gmail.com>
2020-04-17 19:08:13 +0100

    Restructure Vue Project

---
+++ src/APIService.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8080';
+
+export class APIService {
+
+    constructor(){
+
+    }
+
+    getSomething() {
+        const url = `${API_URL}/`;
+        return axios.get(url).then(response => response.data);
+    }
+
+    getUsers() {
+        const url = `${API_URL}/users`;
+        return axios.get(url).then(response => response).catch(function (error) {
+            console.log(error);
+          });
+    }
+
+}
---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <hr>
+
+    <div class="fondo">
+
+      <button @click="getSomething()">Hola mundo</button>
+      <button @click="getUsers()">Usuarios</button>
+      <br>
+      <span>{{ respuesta }}</span>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+
+import {APIService} from './APIService';
+
+const apiService = new APIService();
+
+
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data: function () {
+  return {
+    respuesta: "No hay nada"
+  } 
+},
+  
+  methods: {
+
+    getSomething: function() {
+      apiService.getSomething().then((data) => {
+        this.respuesta = data
+      })
+    },
+    getUsers: function() {
+      apiService.getUsers().then((data) => {
+        this.respuesta = data
+      })
+    },
+
+  },
+  mounted () {
+    
+  },
+  components: {
+    HelloWorld
+  }
+
+  
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.fondo{
+background-color: lightgrey;
+  width: 300px;
+  border: 15px solid green;
+  padding: 50px;
+  margin: 20px;
+
+}
+</style>
